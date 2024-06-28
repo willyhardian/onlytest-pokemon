a3ff6a12b4a022676968357d31fcff305ccd0765
Willy Hardian <hardian.will@gmail.com>
2024-06-28 21:02:38 +0700

    add trainer section

--- pokemon/src/App.jsx
+++ pokemon/src/App.jsx
@@ -8,6 +8,10 @@ function App() {
 
     return (
         <>
+            <div>
+                <h1>Trainer</h1>
+                <p>satoshi doe</p>
+            </div>
             <h1>Pokemon List</h1>
             <p>Pikachu</p>
             <p>Bulbasur</p>
